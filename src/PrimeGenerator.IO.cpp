5eb49fc802d15c470c155c461db4658ae6542ebc
Alex Hicks <alex@alexhicks.net>
2018-11-25 21:22:53 -0500

    Use new data storage method

---
+++ src/PrimeGenerator.IO.cpp
@@ -0,0 +1,47 @@
+#include <experimental/filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include "PrimeGenerator.hpp"
+#include "util.hpp"
+
+#define DATA_INDEX_LIMIT 100000
+
+namespace fs = std::experimental::filesystem;
+
+bool PrimeGenerator::load() {
+  if (!fs::is_directory("data")) {
+    return false;
+  }
+  for (auto& p : fs::directory_iterator("data")) {
+    std::ifstream stream("data/" + p.path().filename().string());
+    std::string line;
+    while (std::getline(stream, line)) {
+      Prime prime;
+      std::istringstream str_stream(line);
+      while (str_stream >> prime) {
+        this->primes.push_back(prime);
+      }
+    }
+    stream.close();
+  }
+  log("Loaded " + std::to_string(this->primes.size()) + " primes.");
+  return true;
+}
+
+void PrimeGenerator::save() {
+  if (!fs::is_directory("data")) {
+    fs::create_directory("data");
+  }
+  Prime last_index = 0;
+  for (auto& p : fs::directory_iterator("data")) {
+    last_index = std::stoi(p.path().filename().string());
+  }
+  for (unsigned int k = last_index; k < this->primes.size(); k += DATA_INDEX_LIMIT) {
+    std::ofstream stream("data/" + std::to_string(k));
+    for (unsigned int i = k; i < k + DATA_INDEX_LIMIT && i < this->primes.size(); i++) {
+      stream << this->primes[i] << " ";
+    }
+    stream.close();
+  }
+}
--- src/PrimeGenerator.cpp
+++ src/PrimeGenerator.cpp
@@ -1,38 +1,10 @@
 #include <cmath>
-#include <fstream>
-#include <iostream>
-#include <sstream>
 #include "PrimeGenerator.hpp"
-#include "util.hpp"
 
 PrimeGenerator::PrimeGenerator() {
   if (!this->load()) {
     this->primes.push_back(2);
   }
-}
-
-bool PrimeGenerator::load() {
-  std::ifstream stream("primes.txt");
-  if (!stream.is_open()) return false;
-  std::string line;
-  while (std::getline(stream, line)) {
-    Prime prime;
-    std::istringstream str_stream(line);
-    while (str_stream >> prime) {
-      this->primes.push_back(prime);
-    }
-  }
-  log("Loaded " + std::to_string(this->primes.size()) + " primes.");
-  stream.close();
-  return true;
-}
-
-void PrimeGenerator::save() {
-  std::ofstream stream("primes.txt");
-  for (unsigned int i = 0; i < this->primes.size(); i++) {
-    stream << this->primes[i] << " ";
-  }
-  stream.close();
 }
 
 bool PrimeGenerator::isPrime(Prime value) {
